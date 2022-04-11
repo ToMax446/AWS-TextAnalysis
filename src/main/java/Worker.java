e50c77e5b1290c327b7107239243515496f2f657
ToMax446 <tomewla@gmail.com>
2022-04-11 13:25:22 +0300

    11.04 mid work update

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -1,9 +1,8 @@
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.Message;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.*;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -16,34 +15,24 @@ public class Worker {
 
     public static void main(String[] args) {
         SqsClient sqs = SqsClient.builder().region(Region.US_WEST_2).build();
-
-        for (;;)    // loop until we get a message
+        S3Client s3 = S3Client.builder().region(Region.US_WEST_2).build();
+        for (; ; )    // loop until we get a message
         {
+            CreateQueueResponse workerQueueRes =
+                    sqs.createQueue(CreateQueueRequest.builder().queueName("dsp-from-worker-to-manager-queue").build());
+            CreateQueueResponse managerQueueRes =
+                    sqs.createQueue(CreateQueueRequest.builder().queueName("dsp-from-manager-to-worker-queue").build());
+            String fromManagerToWorkerQueueUrl = managerQueueRes.queueUrl();
+            String fromWorkerToManagerQueueUrl = workerQueueRes.queueUrl();
             List<Message> messages =
-                    sqs.receiveMessage(ReceiveMessageRequest.builder().queueUrl(managerLocalQueueUrl).build()).messages();
+                    sqs.receiveMessage(ReceiveMessageRequest.builder().queueUrl(fromManagerToWorkerQueueUrl).build()).messages();
             if (!messages.isEmpty()) {
                 String[] message = messages.get(0).body().split("__");
+//                message: "analysis task__bucket__key"
                 if (message[0].equals("analysis task")) {
-                    InputStream sum = s3.getObject(GetObjectRequest.builder().bucket(uniqueBucket).key(message[1]).build());
-                    String text = new BufferedReader(
-                            new InputStreamReader(sum, StandardCharsets.UTF_8)).lines()
-                            .collect(Collectors.joining("\n"));
-
-                    try { // create html file
-                        FileWriter fd = new FileWriter(outputFileName + ".html");
-                        fd.write(text);
-                        fd.close();
-                        if (args.length > 3 && args[3].equals("terminate")) //if we are on termination mode send a termination message to the manager
-                            sqs.sendMessage(SendMessageRequest.builder().queueUrl(localManagerQueueUrl).messageBody("terminate").build());
-                        break; //break after
-                    } catch (IOException e) {
-                        System.out.println("An error occurred.");
-                        exit(1);
-                    }
+                    InputStream sum = s3.getObject(GetObjectRequest.builder().bucket(message[1]).key(message[2]).build());
                 }
             }
-        }
-
-
-    }
-}
+            // send a message from the worker to the manager.
+            sqs.sendMessage(SendMessageRequest.builder().queueUrl(fromWorkerToManagerQueueUrl).messageBody("done OCR task" + Delimiter + parts1[0] + Delimiter + text + Delimiter + parts1[1]).build());
+            // delete the message f
